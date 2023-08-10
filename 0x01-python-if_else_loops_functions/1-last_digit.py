4d5a820da112b4b01763342c61e6cac0ff860f05
Tefe Mebuiefene <tefeglory.official@gmail.com>
2023-08-10 16:07:19 -0700

    task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 mul = 1
 if (number < 0):
     mul = -1
-num = (number % 10) * mul
+num = ((number * mul) % 10) * mul
 if (num < 6 and num != 0):
     print(f"Last digit of {number:d} is {num:d} and is less than 6 and not 0")
 elif (num > 5):
