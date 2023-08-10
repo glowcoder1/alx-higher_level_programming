b465065772bdc0e3ddbce561932283a1b3b6bfeb
Tefe Mebuiefene <tefeglory.official@gmail.com>
2023-08-10 16:50:43 -0700

    task 4 and 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
-for num in range(99):
-    print("{:02d},".format(num), end =" ")
-print("{:d}".format(99))
+for num in range(100):
+    if num != 99:
+        print("{:02d},".format(num), end =" ")
+    else:
+        print("{:d}".format(num))
