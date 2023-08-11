b3a3dfa9f092359c8562b2c27cf0bf34972449eb
Tefe Mebuiefene <tefeglory.official@gmail.com>
2023-08-11 16:48:44 -0700

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,7 @@ import hidden_4
 
 if __name__ == "__main__":
     names = dir(hidden_4)
+    names = sorted(names)
     for name in names:
         if name[:2] != "__":
             print("{:s}".format(name))
