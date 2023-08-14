f14828f6d40bd1209a1c869fec3c7b67a25fb035
Tefe Mebuiefene <tefeglory.official@gmail.com>
2023-08-14 15:08:06 -0700

    tasl 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    count = len(my_list)
+    if idx < 0 or count == 0 or idx > (count - 1):
+        return None
+    return my_list[idx]
+
+    
