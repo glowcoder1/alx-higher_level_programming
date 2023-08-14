fb8fec1fd8d3fb0dd0f3e0763e990417e9f91c77
Tefe Mebuiefene <tefeglory.official@gmail.com>
2023-08-14 16:17:33 -0700

    task 4:

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,7 +5,7 @@ def new_in_list(my_list, idx, element):
     new_l = []
     for val in my_list:
         new_l.append(val)
-    if idx < 0 or count == 0 or idx > (count -1):
+    if idx < 0 or count == 0 or idx > (count - 1):
         return new_l
     new_l[idx] = element
     return new_l
