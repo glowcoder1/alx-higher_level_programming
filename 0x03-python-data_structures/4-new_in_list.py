ded60f2d433d2f98f82e74162a4795d246d1d534
Tefe Mebuiefene <tefeglory.official@gmail.com>
2023-08-14 15:59:10 -0700

    task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    count = len(my_list)
+    new_l = []
+    for val in my_list:
+        new_l.append(val)
+    if idx < 0 or count == 0 or idx > (count -1):
+        return new_l
+    new_l[idx] = element
+    return new_l
