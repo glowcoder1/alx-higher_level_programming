b6b2fbfee5a487ba917f55570eb39799393729a0
Tefe Mebuiefene <tefeglory.official@gmail.com>
2023-08-16 04:03:13 -0700

    fixed task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,9 @@
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
         count = len(x)
+        if count == 0:
+            print(" ")
+            break
         for i in range(count):
             length = count - 1
             if i < (length):
