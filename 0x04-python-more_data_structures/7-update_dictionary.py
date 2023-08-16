605deb79b8cb1dbfc40ff727a7f81ad1182a5c14
Tefe Mebuiefene <tefeglory.official@gmail.com>
2023-08-16 13:57:45 -0700

    task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    new_dict = {}
-    if a_dictionary:
-        new_dict = {key: value for (key, value) in a_dictionary.items()}
-    new_dict[key] = value
-    return new_dict
+    if a_dictionary is None:
+        a_dictionary = {}
+    a_dictionary[key] = value
+    return a_dictionary
