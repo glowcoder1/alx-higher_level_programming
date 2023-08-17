5c054f0826b34ed8338f7fd765b4863021ffe853
Tefe Mebuiefene <tefeglory.official@gmail.com>
2023-08-17 02:17:22 -0700

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 for i in range(9):
-     print(", ".join("{}".format(str(i) + str(j)) for j in range(i + 1, 10)), end=" ")
+     print(" ".join("{},".format(str(i) + str(j)) for j in range(i + 1, 10)), end=" ")
 print("")
