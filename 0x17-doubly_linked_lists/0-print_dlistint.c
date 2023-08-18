6e6b77ed03445c8419b41e560f1930b15b60e278
Tefe Mebuiefene <tefeglory.official@gmail.com>
2023-08-18 07:18:53 -0700

    added header file, task 0 and readme

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistin t- function that prints all the elements of a dlistint_t list
+ * @h: pointer to head of the list
+ * Return: counts of the elements in list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+	const dlistint_t *curr = h;
+
+	while (curr != NULL)
+	{
+		printf("%i\n", curr->n);
+		count++;
+		curr = curr->next;
+	}
+	return (count);
+}
