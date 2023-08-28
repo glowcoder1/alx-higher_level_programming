57973e8666181da914aceed23f3f14ad77890eca
Tefe Mebuiefene <tefeglory.official@gmail.com>
2023-08-28 16:15:22 -0700

    task 0 fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             elem_printed += 1
-        except:
+        except Exception:
             continue
     print()
     return elem_printed
