d18cd35a06abc7b7f9380f08567e836dc4e6b615
Tefe Mebuiefene <tefeglory.official@gmail.com>
2023-09-11 16:07:58 -0700

    task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,4 +6,11 @@ lookup module
 
 
 def lookup(obj):
+    """
+    lookup takes a class and returns list of attr and mtds
+    Parameter:
+    obj
+    Returns:
+    list of attr and mtds
+    """
     return(dir(obj))
