7441483962206c4b34499476517b2def02aa2319
Tefe Mebuiefene <tefeglory.official@gmail.com>
2023-09-11 16:39:54 -0700

    task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+provides inherits_from func
+"""
+
+def inherits_from(obj, a_class):
+    """
+    function that returns True if the object is an instance of a class
+    that inherited (directly or indirectly) from the specified class
+
+    Parameters: obj, a_class
+    Returns: true or false
+
+    """
+    if issubclass(a_class, type(obj)):
+        return True
+    return False
