46d5bd39fc63f0af0e63e8b78d66047038f6e445
Tefe Mebuiefene <tefeglory.official@gmail.com>
2023-09-11 06:40:37 -0700

    added perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -40,4 +40,6 @@ class Rectangle:
         return self.__height * self.__width
 
     def perimeter(self):
+        if self.__width is 0 or self.__height is 0:
+            return 0
         return (self.__height * 2) + (self.__width * 2)
