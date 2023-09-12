79f2a18c6017beed20b0f1ad856a670348c21085
Tefe Mebuiefene <tefeglory.official@gmail.com>
2023-09-12 16:33:50 -0700

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+function that writes a string to a text file
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    write_file
+    function that writes a string to a text file
+    Parameters: filename, text
+    Returns: no of chars written
+    """
+
+   with open(filename, "w") as f:
+       charsWritten = f.write(text)
+       return charsWritten
