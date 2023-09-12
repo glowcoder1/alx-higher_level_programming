19f10ba40110dcc6585bf59848d90a2084132e02
Tefe Mebuiefene <tefeglory.official@gmail.com>
2023-09-12 16:19:01 -0700

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,10 @@ module for read_file
 
 
 def read_file(filename=""):
+    """
+    read_file
+    Parameters: filename
+    Returns: None, prints to stdout
+    """
     with open(filename) as f:
         print(f.read())
