dc5514add9117892d4ceb46fe34e8472dbf8fe75
Tefe Mebuiefene <tefeglory.official@gmail.com>
2023-09-11 17:02:39 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+base geometry class module
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
