b9ab06d3c873f11b475e19aab9fe79cc9105eeec
Tefe Mebuiefene <tefeglory.official@gmail.com>
2023-09-12 17:15:38 -0700

    task 4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -8,11 +8,11 @@ from_json_string
 import json
 
 
-def from_json_string(my_obj):
+def from_json_string(my_str):
     """
     from_json_string
     function that returns the JSON representation of an object (string)
     Parameters: my_obj
     Returns: JSON
     """
-    return json.load(my_obj)
+    return json.loads(my_str)
