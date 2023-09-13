5714b384c19d2da745d6782748885ef801896647
Tefe Mebuiefene <tefeglory.official@gmail.com>
2023-09-13 16:41:44 -0700

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,14 +4,15 @@
 print sorted
 """
 
+
 class MyList(list):
     """MyList class"""
     def print_sorted(self):
-    """
-    prints sorted list
-    Paramters: self
-    Returns: none
-    """
-    temp = self.copy()
-    temp.sort()
-    print(temp)
+        """
+        prints sorted list
+        Paramters: self
+        Returns: none
+        """
+        temp = self.copy()
+        temp.sort()
+        print(temp)
