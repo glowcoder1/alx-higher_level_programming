ded90f63bf626d52040c0136d6a7c124fc2fe3f0
glowcoder1 <tefeglory.official@gmail.com>
2023-11-08 09:31:52 -0800

    task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -9,6 +9,7 @@ class Rectangle:
     """Rectangle class with attributes width and height"""
 
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
