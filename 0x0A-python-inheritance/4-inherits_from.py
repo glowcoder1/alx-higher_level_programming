ab3eede9b87d04439451666d93293ab961eb6eb5
glowcoder1 <tefeglory.official@gmail.com>
2023-11-08 08:33:32 -0800

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -14,6 +14,6 @@ def inherits_from(obj, a_class):
     Returns: true or false
 
     """
-    if isinstance(obj, a_class):
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
