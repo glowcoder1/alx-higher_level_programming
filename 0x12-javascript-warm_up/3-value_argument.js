6dc6a16f4bade51817bb0b2d969d467e5ff93f33
root <tefeglory.official@gmail.com>
2024-01-09 00:50:01 +0100

    task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,9 +2,7 @@
 
 const args = process.argv.slice(2);
 if (args[0]) {
-  args.forEach((arg, i) => {
-    console.log(arg);
-  });
+  console.log(args[0]);
 } else {
   console.log('No argument');
 }
