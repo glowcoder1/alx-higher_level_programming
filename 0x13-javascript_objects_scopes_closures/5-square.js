f63c39ee7d9578175f9d06d0dbd21bcf240c466b
root <tefeglory.official@gmail.com>
2024-01-09 13:42:37 +0100

    task 5

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -3,10 +3,6 @@ const Rectangle = require('./4-rectangle.js');
 
 module.exports = class Square extends Rectangle {
   constructor (size) {
-    super();
-    if (size > 0) {
-      this.height = size;
-      this.width = size;
-    }
+    super(size, size);
   }
 };
