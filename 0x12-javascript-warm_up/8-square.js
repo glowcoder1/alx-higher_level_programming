da5d254e0b15e09d6482f53bce262e1b9bad4e34
root <tefeglory.official@gmail.com>
2024-01-09 09:22:35 +0100

    task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const arg1 = process.argv.slice(2)[0];
+
+if (isNaN(arg1)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < arg1; i++) {
+    let sq = '';
+    for (let j = 0; j < arg1; j++) {
+      sq = sq + '#';
+    }
+    console.log(sq);
+  }
+}
