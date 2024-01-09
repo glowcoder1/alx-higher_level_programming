51aa021eb725426c88a58449f2837cf7b7b49b47
root <tefeglory.official@gmail.com>
2024-01-09 14:57:09 +0100

    task 12

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const { dict } = require('./101-data.js');
+
+const newDict = {};
+for (const key in dict) {
+  const val = dict[key];
+  if (newDict[val]) {
+    newDict[val] = newDict[val].push(key);
+  } else {
+    newDict[val] = [key];
+  }
+}
+console.log(newDict);
