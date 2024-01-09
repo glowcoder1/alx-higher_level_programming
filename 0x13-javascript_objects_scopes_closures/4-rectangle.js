e7355e0974fa4abff812d0ce78b493b72d20bdf3
root <tefeglory.official@gmail.com>
2024-01-09 13:27:27 +0100

    task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        let rect = '';
+        for (let j = 0; j < this.width; j++) {
+          rect = rect + 'X';
+        }
+        console.log(rect);
+      }
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
