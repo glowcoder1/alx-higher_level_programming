1e18a5201e8892cb14b30f1e252e6d0bce74756f
root <tefeglory.official@gmail.com>
2024-05-02 22:46:38 +0100

    task 4

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,11 +1,5 @@
 $('#toggle_header').click((e) => {
   e.preventDefault();
   const header = $('header');
-  if (header.hasClass('red')) {
-    header.removeClass('red');
-    header.addClass('green');
-  } else {
-    header.removeClass('green');
-    header.addClass('red');
-  }
+  header.toggleClass('red green');
 });
