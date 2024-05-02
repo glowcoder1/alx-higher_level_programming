4a56257dd51b4d62c75fb608a77acf3d5aec459a
root <tefeglory.official@gmail.com>
2024-05-02 22:26:16 +0100

    7, 8, 9

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,9 +1,11 @@
-fetch('https://swapi-api.alx-tools.com/api/films/?format=json')
-  .then((resp) => {
-    return resp.json();
-  })
-  .then((val) => {
-    val.results.forEach(data => {
-      $('#list_movies').append(`<li>${data.title}</li>`);
-    });
+$(function () {
+  $.ajax({
+    type: 'GET',
+    url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
+    success: function (data) {
+      $.each(data.results, function (i, movie) {
+        $('UL#list_movies').append(`<li>${movie.title}</li>`);
+      });
+    }
   });
+});
